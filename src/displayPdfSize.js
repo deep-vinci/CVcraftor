8249972e8cacbe6ce56fdb0b485c486d5bef29dd
Deepak <deepakworkpc@gmail.com>
2024-07-19 21:51:21 +0530

    feat: add the sizing of a4 Size Paper to the child of displayPdf using the ratio calculations

--- src/displayPdfSize.js
+++ src/displayPdfSize.js
@@ -2,6 +2,9 @@ window.addEventListener("load", () => {
     let displayPdf = document.querySelector(".display-pdf")
     let a4SizePaper = document.querySelector(".a4-size");
 
-    
-    console.log(a4SizePaper.getBoundingClientRect())
+    let displayPdfContainer = displayPdf.getBoundingClientRect();
+
+    // a4 sizing of the a4SizePaper
+    a4SizePaper.style.width = `${displayPdfContainer.height / 1.414}px`;
+    a4SizePaper.style.height = `${displayPdfContainer.height}px`;
 })
